b4ba2b78386e18a80195a222d81ed4d1c45401c6
93Kamuran <enginkamuran@gmail.com>
2020-11-29 16:48:16 +0300

    SyncTransactions and InstallS3Integration commands refatored.

--- src/Commands/InstallS3IntegrationPackage.php
+++ src/Commands/InstallS3IntegrationPackage.php
@@ -5,6 +5,9 @@ namespace Inensus\OdysseyS3Integration\Commands;
 
 
 use Illuminate\Console\Command;
+use Inensus\OdysseyS3Integration\Models\OdysseyS3SyncObjectTag;
+use Inensus\OdysseyS3Integration\Services\MenuItemService;
+use Inensus\OdysseyS3Integration\Services\S3AuthorizationService;
 
 class InstallS3IntegrationPackage extends Command
 {
@@ -12,19 +15,28 @@ class InstallS3IntegrationPackage extends Command
     protected $signature = 'odyssey-s3:install';
     protected $description = 'Install the Odyssey S3 Integration Package';
 
+    private $s3AuthorizationService;
+    private $menuItemService;
+    private $odysseyS3SyncObjectTag;
+    public function __construct(
+        MenuItemService $menuItemService,
+        S3AuthorizationService $s3AuthorizationService,
+        OdysseyS3SyncObjectTag $odysseyS3SyncObjectTag
+    )
+    {
+        parent::__construct();
+        $this->menuItemService = $menuItemService;
+        $this->s3AuthorizationService=$s3AuthorizationService;
+        $this->odysseyS3SyncObjectTag = $odysseyS3SyncObjectTag;
+    }
+
     public function handle(): void
     {
         $this->info('Installing Odyssey S3 Integration Package\n');
 
-        $this->info('Copying config file');
+       $this->info('Copying migrations\n');
         $this->call('vendor:publish', [
-            '--provider' => "Inensus\OdysseyS3Integration\OdysseyS3IntegrationServiceProvider",
-            '--tag' => "config"
-        ]);
-
-        $this->info('Copying migrations\n');
-        $this->call('vendor:publish', [
-            '--provider' => "Inensus\OdysseyS3Integration\OdysseyS3IntegrationServiceProvider",
+            '--provider' => "Inensus\OdysseyS3Integration\Providers\OdysseyS3IntegrationServiceProvider",
             '--tag' => "migrations"
         ]);
 
@@ -34,6 +46,32 @@ class InstallS3IntegrationPackage extends Command
 
         ]);
 
+        $this->call('plugin:add', [
+            'name' => "Odyssey-S3",
+            'composer_name' => "inensus/odyssey-s3-integration",
+            'description' => "Enables to push data to OdysseyEnergy via AmazonS3",
+        ]);
+        $this->call('routes:generate');
+
+        $menuItems = $this->menuItemService->createMenuItems();
+        $this->call('menu-items:generate', [
+            'menuItem' => $menuItems['menuItem'],
+            'subMenuItems' => $menuItems['subMenuItems'],
+        ]);
+
+        $this->odysseyS3SyncObjectTag->newQuery()->create([
+            'name'=>'Transaction'
+        ]);
+        $this->info('Transaction tag created..');
+
+        $this->info('Copying vue files\n');
+
+        $this->call('vendor:publish', [
+            '--provider' => "Inensus\OdysseyS3Integration\Providers\OdysseyS3IntegrationServiceProvider",
+            '--tag' => "vue-components"
+        ]);
+
+        $this->call('sidebar:generate');
 
         $this->info('Package installed successfully..');
     }
